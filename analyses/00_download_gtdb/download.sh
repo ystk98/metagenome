f65e14e5a9cfa6a5b48fa78e9ac0c2e9e7b75df3
yasutake <yasutake.masamitsu@gmail.com>
2025-12-11 13:43:55 +0900

    Re-download GTDB

--- analyses/00_download_gtdb/download.sh
+++ analyses/00_download_gtdb/download.sh
@@ -6,24 +6,26 @@ cd "$(dirname "$0")" || exit
 OUT_DIR="../../data/gtdb"
 
 LOG_DIR="logs"
-LOG_FILE="${LOG_DIR}/$(date +%Y-%m-%d)_download.log"
+LOG_FILE="${LOG_DIR}/$(date +%Y-%m-%d_%H-%M-%S)_download.log"
+mkdir -p "$LOG_DIR"
 
 URL_BASE="https://data.ace.uq.edu.au/public/gtdb/data/releases/release226/226.0/"
 URL_REPS=${URL_BASE}"genomic_files_reps/gtdb_genomes_reps_r226.tar.gz"
+VERSION="226.0"
 
-# mkdir -p "$OUT_DIR"
-mkdir -p "$LOG_DIR"
+TARGET_DIR="$OUT_DIR/$VERSION/genomic_files_reps"
+mkdir -p "$TARGET_DIR"
 
 log() {
     echo "$1" | tee -a "$LOG_FILE"
 }
 
 log "Downloading GTDB representative genomes..." # High priority
-wget -c --show-progress -a "$LOG_FILE" -P "$OUT_DIR" "$URL_REPS"
+wget -c --show-progress -a "$LOG_FILE" -P "$TARGET_DIR" "$URL_REPS"
 log "Priority download complete."
 
 log "Downloading all GTDB files except genomic_files_all/..."
-wget -r -c -np -nd --show-progress -a "$LOG_FILE" \
+wget -r -c -np -nH --cut-dirs=5 --show-progress -a "$LOG_FILE" \
   -R "index.html*" \
   -X "genomic_files_all" \
   -P "$OUT_DIR" \
